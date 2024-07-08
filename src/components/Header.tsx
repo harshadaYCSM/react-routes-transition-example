f7d5eefbc80ccdc75a592b7aad21d16deae63a68
Harshada <charsh18@gmail.com>
2024-07-08 11:19:12 +0200

    Installed prettier, optimized Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,24 +1,39 @@
 // src/pages/About.tsx
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import { Button, Typography } from '@mui/material';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Button, Typography } from "@mui/material";
 
 const Header: React.FC = () => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
+  const [activeRoute, setActiveRoute] = useState<string>("");
 
-    return (
-        <div>
-            <Button variant="contained" color="primary" onClick={() => navigate('/')}>
-                Home
-            </Button>
-            <Button variant="contained" color="primary" onClick={() => navigate('/contact')}>
-                Contact
-            </Button>
-            <Button variant="contained" color="primary" onClick={() => navigate('/about')}>
-                About
-            </Button>
-        </div>
-    );
+  const routes: { [key: string]: string } = {
+    home: "/",
+    contact: "/contact",
+    about: "/about",
+  };
+  const handleRouteChange = (route: string) => {
+    setActiveRoute(route);
+    navigate(routes[route]);
+  };
+
+  const setColor = (route: string) => {
+    return activeRoute === route ? "secondary" : "primary";
+  };
+  return (
+    <div>
+      {Object.keys(routes).map((route) => (
+        <Button
+          key={route}
+          variant="contained"
+          color={setColor(route)}
+          onClick={() => handleRouteChange(route)}
+        >
+          {route}
+        </Button>
+      ))}
+    </div>
+  );
 };
 
 export default Header;
